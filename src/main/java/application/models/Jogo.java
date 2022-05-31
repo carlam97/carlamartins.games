67223025108659e39db84e474f3dbd6c14887d59
carlam97 <carmartins04@hotmail.com>
2022-05-31 17:31:21 +0000

    manytoone

--- src/main/java/application/models/Jogo.java
+++ src/main/java/application/models/Jogo.java
@@ -5,7 +5,7 @@ import javax.persistence.Table;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 
@@ -19,7 +19,8 @@ public class Jogo {
     private int id;
     private String titulo;
 
-    @ManyToMany(mappedBy = "id_genero")
+    @ManyToOne
+    @JoinColumn(name="id_genero")
     private Genero genero;
 
     @ManyToMany
